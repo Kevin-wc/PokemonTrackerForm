502f14adb1b7bc1792747b6c90b920b9d4411aa5
kevin <kevin.gonzalezr203@gmail.com>
2025-09-15 04:20:01 -0500

    Finished Layout with TextViews and TextEdits. Currently working in adding functionality.

--- app/src/main/java/com/example/pokemontrackerform/PokedexActivity.java
+++ app/src/main/java/com/example/pokemontrackerform/PokedexActivity.java
@@ -1,6 +1,11 @@
 package com.example.pokemontrackerform;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,29 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class PokedexActivity extends AppCompatActivity {
+
+    EditText numberET;
+    EditText nameET;
+    EditText speciesET;
+    RadioButton femaleB;
+    RadioButton maleB;
+    RadioButton unknownB;
+    EditText heightET;
+    EditText weightET;
+    Spinner levelS;
+    EditText hpET;
+    EditText attackET;
+    EditText defenseET;
+    Button resetB;
+    Button saveB;
+
+    View.OnClickListener buttonListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +48,23 @@ public class PokedexActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        numberET = findViewById(R.id.numberET);
+        nameET = findViewById(R.id.nameET);
+        speciesET = findViewById(R.id.speciesET);
+        femaleB = findViewById(R.id.femaleB);
+        maleB = findViewById(R.id.maleB);
+        unknownB = findViewById(R.id.unknownB);
+        heightET = findViewById(R.id.heightET);
+        weightET = findViewById(R.id.weightET);
+        levelS = findViewById(R.id.levelS);
+        hpET = findViewById(R.id.hpET);
+        attackET = findViewById(R.id.attackET);
+        defenseET = findViewById(R.id.defenseET);
+        resetB = findViewById(R.id.resetB);
+        saveB = findViewById(R.id.saveB);
+
+
+
     }
 }
